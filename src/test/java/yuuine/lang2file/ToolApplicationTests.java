ec17fc6d601291a2040905a9151820523c33c54c
yunqing <jzhang0703@163.com>
2026-02-20 15:36:42 +0800

    ci: 更新CI工作流并添加测试配置

--- src/test/java/yuuine/lang2file/ToolApplicationTests.java
+++ src/test/java/yuuine/lang2file/ToolApplicationTests.java
@@ -2,8 +2,10 @@ package yuuine.lang2file;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ToolApplicationTests {
 
     @Test
